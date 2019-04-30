7feea17ce9b8dcc8617c01357ef908241bf3cd10
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-04-30 15:31:15 +0200

    Update fuzz tests to parse with all grammar variants

--- fuzz/test.ml
+++ fuzz/test.ml
@@ -2,13 +2,15 @@ let test_case ~input ?name () =
   let test_name = match name with Some n -> n | None -> input in
   let test_fun () =
     let input = Cstruct.of_hex input in
-    ignore @@ Any.decode_ber input
+    ignore @@ Any.decode_ber input;
+    ignore @@ Any.decode_ber' input;
+    ignore @@ Any.decode_ber'' input
   in
   (test_name, `Quick, test_fun)
 
 
 let test_cases =
-  [ test_case ~input:"030105" () (* Works with [bit_string_cs] but fails with [bit_string] *)
+  [ test_case ~input:"030105" ()
   ]
 
 let () = Alcotest.run "AFL fuzz test cases" [("", test_cases)]
